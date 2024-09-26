2d0b356d49a1a8055403b2d4e0ffd02278ad6627
o0Mink0o <141802449+o0Mink0o@users.noreply.github.com>
2024-09-26 14:53:00 +0700

    db

--- src/app/libs/DB.ts
+++ src/app/libs/DB.ts
@@ -1,38 +1,29 @@
 import _ from "lodash";
 import { LowSync, MemorySync } from "lowdb";
 import { JSONFileSync } from "lowdb/node";
-
 export interface Room {
   roomId: string;
   roomName: string;
 }
-
 export interface Message {
   roomId: string;
   messageId: string;
   messageText: string;
 }
-
 export interface User {
   username: string;
   password: string;
   role: "ADMIN" | "SUPER_ADMIN";
 }
-
-export interface Database{
+export interface Database {
   rooms: Room[];
   messages: Message[];
   users: User[];
 }
-
-export interface Payload{
-  token: string;
-  user: User;
-  tokenExpired: boolean;
+export interface Payload {
+  username: string;
+  role: string;
 }
-
-
-
 const originalDB = {
   rooms: [
     {
@@ -66,15 +57,12 @@ const originalDB = {
     { username: "user2", password: "5678", role: "SUPER_ADMIN" },
   ],
 };
-
 const onProduction = process.env.NODE_ENV === "production";
-
 const adapter = onProduction
   ? new MemorySync()
   : new JSONFileSync("DatabaseFile.json");
 let lowDB = new LowSync(adapter, originalDB);
 export let DB = onProduction ? _.cloneDeep(originalDB) : lowDB.data;
-
 export function resetDB() {
   if (onProduction) {
     DB = _.cloneDeep(originalDB);
@@ -84,14 +72,12 @@ export function resetDB() {
     lowDB.write();
   }
 }
-
 export function readDB() {
   if (!onProduction) {
     lowDB.read();
     DB = lowDB.data;
   }
 }
-
 export function writeDB() {
   if (!onProduction) {
     lowDB.write();
