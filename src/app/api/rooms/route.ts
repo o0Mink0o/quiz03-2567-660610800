4bf71923ecc9c0c5ecc5a49ff7f650886852f8ea
o0Mink0o <141802449+o0Mink0o@users.noreply.github.com>
2024-09-26 14:57:27 +0700

    room

--- src/app/api/rooms/route.ts
+++ src/app/api/rooms/route.ts
@@ -1,50 +1,4 @@
 import { DB, readDB, writeDB } from "@lib/DB";
-import { checkToken } from "@lib/checkToken";
-import { nanoid } from "nanoid";
-import { NextRequest, NextResponse } from "next/server";
-
-export const GET = async () => {
-  readDB();
-  return NextResponse.json({
-    ok: true,
-    //rooms:
-    //totalRooms:
-  });
-};
-
-export const POST = async (request: NextRequest) => {
-  const payload = checkToken();
-
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Invalid token",
-  //   },
-  //   { status: 401 }
-  // );
-
-  readDB();
-
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: `Room ${"replace this with room name"} already exists`,
-  //   },
-  //   { status: 400 }
-  // );
-
-  const roomId = nanoid();
-
-  //call writeDB after modifying Database
-  writeDB();
-
-  return NextResponse.json({
-    ok: true,
-    //roomId,
-    message: `Room ${"replace this with room name"} has been created`,
-  });
-};/*
-import { DB, readDB, writeDB } from "@lib/DB";
 import { Database,Payload } from "@lib/DB";
 import { checkToken } from "@lib/checkToken";
 import { nanoid } from "nanoid";
@@ -59,6 +13,8 @@ export const GET = async () => {
     totalRooms: (<Database>DB).rooms.length,
   });
 };
+
+
 
 export const POST = async (request: NextRequest) => {
   const payload = checkToken();
@@ -120,4 +76,5 @@ export const POST = async (request: NextRequest) => {
     //roomId,
     message: `Room ${"replace this with room name"} has been created`,
   });
-}; */
+};
+
